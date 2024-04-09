e3a831bccc620427efbbd2dc69e8c3a75ddaf6b8
guilhermevini2013 <guilhermevini2013@hotmail.com>
2024-04-09 10:10:28 -0300

    criado o construtor da InformationHotel.java

--- src/main/java/org/guilhermedev/hotelbooking/models/hotel/InformationHotel.java
+++ src/main/java/org/guilhermedev/hotelbooking/models/hotel/InformationHotel.java
@@ -7,6 +7,11 @@ public class InformationHotel {
     private Integer totalBookings;
     private Double totalEvaluations;
 
+    public InformationHotel() {
+        totalBookings = 0;
+        totalEvaluations = 0.0;
+    }
+
     public Integer getTotalBookings() {
         return totalBookings;
     }
